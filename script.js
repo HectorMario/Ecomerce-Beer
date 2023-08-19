c4796fb90653a8f1dd3a5859f9e0402e44335d24
Hector Perez <hector.mario.peraz@gmail.com>
2023-08-19 09:57:12 +0200

    finish modal and funtion filter

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 const right = document.querySelector(".right-button");
 const left = document.querySelector(".left-button");
+const modal = document.querySelector("#modal");
+const filter = document.querySelector("#filter");
+const closeModal = document.querySelector(".close");
+const inputs = document.querySelectorAll("input");
+const activateFilter = document.getElementById("activateFilter")
+const arrayFilter = [];
 var products = getProducts();
 var source = document.getElementById("render").innerHTML;
 var template = Handlebars.compile(source);
 let page = 1;
-function getProducts(){
+function getProducts() {
   fetch("../products.json")
-  .then(response => response.json())
-  .then(data => {
-    if (page == 1) {
-      let paginator = data.products.slice(0,8);
-      var html = template({products:[...paginator]});
-      document.querySelector("#output").innerHTML = html;
-    }else{
-      let paginator = data.products.slice(8,10);
-      var html = template({products:[...paginator]});
-      document.querySelector("#output").innerHTML = html;
-    }
-  })
-  .catch(error => {
-    console.error("Error al cargar el archivo JSON:", error);
-  });
+    .then(response => response.json())
+    .then(data => {
+      if (page == 1) {
+        let paginator = data.products.slice(0, 8);
+        var html = template({ products: [...paginator] });
+        document.querySelector("#output").innerHTML = html;
+      } else {
+        let paginator = data.products.slice(8, 10);
+        var html = template({ products: [...paginator] });
+        document.querySelector("#output").innerHTML = html;
+      }
+    })
+    .catch(error => {
+      console.error("Error al cargar el archivo JSON:", error);
+    });
 }
 
 left.addEventListener('click', () => {
@@ -39,4 +45,44 @@ right.addEventListener('click', () => {
   right.setAttribute('disabled');
   left.classList.remove('disabled');
   left.removeAttribute('disabled');
+})
+
+filter.addEventListener('click', () => {
+  modal.classList.add('show');
+})
+
+closeModal.addEventListener('click', () => {
+  modal.classList.remove('show');
+})
+
+inputs.forEach(element => {
+  element.addEventListener('click', (input) => {
+    if(input.target.checked){
+      arrayFilter.push(input.target.defaultValue);
+    } else {
+      let index = arrayFilter.indexOf(input.target.defaultValue);
+      arrayFilter.splice(index, 1);
+    }
+    console.log(arrayFilter);
+  })
+});
+
+activateFilter.addEventListener('click', () => {
+  fetch("../products.json")
+    .then(response => response.json())
+    .then(data => {
+      let paginator = data.products.map(x => {
+        let convertString = x.filterId.toString();
+        if (arrayFilter.includes(convertString) ) {
+          return x;
+        }
+      }).filter(x => x !== undefined);
+      console.log(paginator);
+      var html = template({ products: [...paginator] });
+      document.querySelector("#output").innerHTML = html;
+
+    })
+    .catch(error => {
+      console.error("Error to downloas JSON:", error);
+    });
 })
